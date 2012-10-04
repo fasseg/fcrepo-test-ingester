f42300e1a7a3a217a03b2f5203917c2619d58da1
fasseg <frank.asseg@googlemail.com>
2012-10-04 15:24:37 +0200

    added update test

--- src/main/java/org/fcrepo/test/TestIngester.java
+++ src/main/java/org/fcrepo/test/TestIngester.java
@@ -6,6 +6,8 @@ import java.io.OutputStream;
 import java.net.URI;
 import java.security.Principal;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.RandomStringUtils;
@@ -15,7 +17,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
@@ -44,11 +48,45 @@ public class TestIngester {
 		TestIngester ingester = new TestIngester(uri, user, pass);
 		try {
 			String objectId = ingester.ingestObject("test-1");
+			System.out.println("ingested test object");
 			for (int i = 0; i < numDatatstreams; i++) {
 				ingester.ingestDatastream(objectId, "ds-" + (i + 1), size);
 			}
+			System.out.println("ingested datastreams");
+			ingester.updateAllDatastreams(objectId, size);
+			System.out.println("updated datastreams");
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private void updateAllDatastreams(String objectId, int size)
+			throws Exception {
+		HttpGet get = new HttpGet(this.fedoraUri + "/objects/" + objectId
+				+ "/datastreams?format=xml");
+		HttpResponse resp = client.execute(get);
+		if (resp.getStatusLine().getStatusCode() != 200) {
+			throw new Exception("Unable to get object with id " + objectId
+					+ ". fedora returned " + resp.getStatusLine());
+		}
+		String xml = IOUtils.toString(resp.getEntity().getContent());
+		get.releaseConnection();
+		Matcher m = Pattern.compile("datastream dsid=\"ds-.*?\"").matcher(xml);
+		while (m.find()) {
+			String dsId = xml.substring(m.start() + 17, m.end() - 1);
+			updateDatastream(objectId, dsId, size);
+		}
+	}
+
+	private void updateDatastream(String objId,String dsId, int size) throws Exception{
+		HttpPut put = new HttpPut(this.fedoraUri + "/objects/" + objId + "/datastreams/" + dsId);
+		put.setEntity(new ByteArrayEntity(getRandomBytes(size)));
+		long start = System.currentTimeMillis();
+		HttpResponse resp = client.execute(put);
+		System.out.println((System.currentTimeMillis() - start));
+		put.releaseConnection();
+		if (resp.getStatusLine().getStatusCode() != 200){
+			throw new Exception("Unabel to update datastream " + dsId + ". Fedora returned " + resp.getStatusLine());
 		}
 	}
 
@@ -56,16 +94,17 @@ public class TestIngester {
 			throws Exception {
 		HttpPost post = new HttpPost(fedoraUri.toASCIIString() + "/objects/"
 				+ objectId + "/datastreams/" + label
-				+ "?versionable=true&controlGroup=M");
+				+ "?versionable=false&controlGroup=M");
 		post.setHeader("Content-Type", "application/octet-stream");
 		post.setEntity(new ByteArrayEntity(getRandomBytes(size)));
 		long start = System.currentTimeMillis();
 		HttpResponse resp = client.execute(post);
 		post.releaseConnection();
 		if (resp.getStatusLine().getStatusCode() != 201) {
-			throw new Exception("Unable to ingest datastream " + label + " fedora returned " + resp.getStatusLine());
+			throw new Exception("Unable to ingest datastream " + label
+					+ " fedora returned " + resp.getStatusLine());
 		}
-		System.out.println("ds ingest took " + (System.currentTimeMillis() - start) + " ms");
+//		System.out.println((System.currentTimeMillis() - start));
 	}
 
 	private byte[] getRandomBytes(int size) {
